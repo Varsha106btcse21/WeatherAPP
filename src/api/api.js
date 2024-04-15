7160b824747d7bb76ef1aa65ae4490644068e40f
Varsha106btcse21 <varsha106btcse21@igdtuw.ac.in>
2024-04-15 23:25:11 +0530

    WeatherApp

---
+++ src/api/api.js
@@ -0,0 +1,11 @@
+const apiKey = '6c773b30d4ed86d70d9f0a115b0c12c8';
+
+const getWeather = async (city) => {
+    return await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
+    .then((res) => res.json())
+    .then((json) => {
+        return json;
+    })
+}
+
+export default getWeather;
